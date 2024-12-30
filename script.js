2f6b050beb3e330b9cc5202df5510882bba42cf7
diwakarstha997 <165546734+diwakarstha997@users.noreply.github.com>
2024-12-30 14:39:06 +1100

    Create operate to perform operation

--- script.js
+++ script.js
@@ -16,4 +16,25 @@ function calculator(){
         return num1 / num2;
     }
 
+    // Operate Determines which operation to perform based on input
+    function operate(operator, num1, num2) {
+        switch(operator){
+            case '+':
+                add(num1, num2);
+                break;
+
+            case '-':
+                substract(num1, num2);
+                break;
+
+            case '*':
+                multiply(num1, num2);
+                break;
+
+            case '/':
+                divide(num1, num2);
+                break;
+                
+        }
+    }
 }
