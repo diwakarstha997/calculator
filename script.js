e4ad0142156e921fbfb29dc7935c0ff28cac0ecf
diwakarstha997 <165546734+diwakarstha997@users.noreply.github.com>
2024-12-30 14:32:37 +1100

    Create functionalites for calculator

---
+++ script.js
@@ -0,0 +1,19 @@
+function calculator(){
+    // Calculator Functionalities
+    function add(num1, num2){
+        return num1 + num2;
+    }
+
+    function substract(num1, num2){
+        return num1 - num2;
+    }
+
+    function multiply(num1, num2){
+        return num1 * num2;
+    }
+
+    function divide(num1, num2) {
+        return num1 / num2;
+    }
+
+}
